5db470cfa4ae2cda7a2fa63fa49542d886163c64
KnightMiner <knightminer4@gmail.com>
2026-05-06 02:10:27 -0400

    Allow swapping materials on tools by sacrificing another tool
    Serves as an early game form of part swapping from an already crafted tool, but more importantly late game its good for swapping ancient tool parts
    Currently no extra cost on ancient tools to do so, may reconsider that later (recipe supports it)

--- src/main/java/slimeknights/tconstruct/tables/recipe/TinkerStationPartSwappingBuilder.java
+++ src/main/java/slimeknights/tconstruct/tables/recipe/TinkerStationPartSwappingBuilder.java
@@ -6,19 +6,40 @@ import lombok.experimental.Accessors;
 import net.minecraft.data.recipes.FinishedRecipe;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.item.crafting.Ingredient;
+import net.minecraft.world.level.ItemLike;
 import slimeknights.mantle.data.loadable.Loadables;
 import slimeknights.mantle.recipe.data.AbstractRecipeBuilder;
+import slimeknights.mantle.recipe.ingredient.SizedIngredient;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.function.Consumer;
 
-/** Builder for {@link TinkerStationPartSwapping} */
+/** Builder for {@link TinkerStationPartSwapping} and {@link ToolMaterialSwappingRecipe} */
 @RequiredArgsConstructor(staticName = "tools")
 public class TinkerStationPartSwappingBuilder extends AbstractRecipeBuilder<TinkerStationPartSwappingBuilder> {
   private final Ingredient tools;
   @Setter
   @Accessors(fluent = true)
   private int maxStackSize = 16;
+  /** Additional requirements beyond the "part" */
+  private final List<SizedIngredient> extraRequirements = new ArrayList<>();
 
+  /** Adds an extra ingredient requirement */
+  public TinkerStationPartSwappingBuilder addExtraRequirement(SizedIngredient ingredient) {
+    extraRequirements.add(ingredient);
+    return this;
+  }
+
+  /** Adds an extra ingredient requirement */
+  public TinkerStationPartSwappingBuilder addExtraRequirement(Ingredient ingredient) {
+    return addExtraRequirement(SizedIngredient.of(ingredient));
+  }
+
+  /** Adds an extra ingredient requirement */
+  public TinkerStationPartSwappingBuilder addExtraRequirement(ItemLike... items) {
+    return addExtraRequirement(SizedIngredient.fromItems(items));
+  }
 
   @Override
   public void save(Consumer<FinishedRecipe> consumer) {
